fe8844b65c86abf466d6df4532729f870c6fb6de
Raphael Procópio <raphalprocopio26@gmail.com>
2026-02-18 16:24:22 -0300

    feat: :memo: Área do circulo

---
+++ Lista de exercicio c#/Exercicio06.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Globalization;
+class Exercicio06
+{
+  static void Main(string[] args)
+{
+    Console.WriteLine("Informe o valor do raio do circulo");
+double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double area = Math.PI * Math.Pow(raio,2);
+Console.WriteLine($"O valor da área do circulo é {area:F4}");
+Console.ReadKey();
+    }  
+}
